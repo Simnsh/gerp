7273c35892493c3b33908de14dbfed42d0ca0f50
Simnsh <149747294+Simnsh@users.noreply.github.com>
2025-04-23 16:15:57 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include "Hash.h"
+#include "FSTree.h"
+
+#include <istream>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+template<typename streamtype>
+void open_or_die(streamtype &stream, string file_name);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cerr << "Usage: ./gerp inputDirectory outputFile" << endl;
+        return EXIT_FAILURE;
+    }
+    ofstream outstream;
+    open_or_die(outstream, argv[2]);
+    Hash h;
+    h.run(argv[1], outstream);
+    
+    outstream.close();
+}
+
+/*
+ * Try to open the given file.
+ * Abort on failure.
+ * If the function returns, caller can assume stream is open.
+ * By using a template, we gain modularity: this function will work
+ * for ifstreams *and* ofstreams.
+ */
+template<typename streamtype>
+void open_or_die(streamtype &stream, string file_name)
+{
+    stream.open(file_name);
+    if (not stream.is_open()) {
+        cerr << "Error: could not open file " << file_name << endl;
+        exit(EXIT_FAILURE);
+    }
+}
